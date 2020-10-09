78b77eee2b16e766850f77e868ea4e3bf3e6bae7
Ben Codrington <benvenda@gmail.com>
2020-10-09 15:22:06 -0400

    Add colour styles to App.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
+import './App.scss';
 import SoundscapeList from './components/SoundscapeList';
 import { Soundscape } from './models/Soundscape';
 import AppHeader from './widgets/AppHeader';
