aacb0cfff8703714caf2f517f0d91ea2f8bb709b
Ben Codrington <benvenda@gmail.com>
2020-11-22 13:54:23 -0500

    Add dummy controls to track list item

--- src/components/TrackList/TrackListItem.tsx
+++ src/components/TrackList/TrackListItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Track } from "../../models/Track";
+import DefaultButton from "../../widgets/buttons/DefaultButton";
 
 import "./TrackListItem.scss";
 
@@ -9,9 +10,38 @@ type TrackListItemProps = {
 
 export default function TrackListItem({ track }: TrackListItemProps) {
   const { trackMetadata } = track;
+  function displaySource() {
+    console.log('TODO');
+  }
+  function toggleMuted() {
+    console.log('TODO');
+  }
+  function toggleIsPlaying() {
+    console.log('TODO');
+  }
+  function remove() {
+    console.log('TODO');
+  }
   return (
     <div>
       <p>{trackMetadata.name}</p>
+      {/* TODO: volume slider */}
+      <DefaultButton
+        onClick={toggleMuted}
+        text="toggle mute"
+      />
+      <DefaultButton
+        onClick={toggleIsPlaying}
+        text="toggle playing"
+      />
+      <DefaultButton
+        onClick={displaySource}
+        text="source"
+      />
+      <DefaultButton
+        onClick={remove}
+        text="x"
+      />
     </div>
   );
 }
