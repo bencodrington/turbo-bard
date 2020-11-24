ffbf704e215059b2d844f0a2d41da157ec12ab06
Ben Codrington <benvenda@gmail.com>
2020-11-23 22:56:18 -0500

    Add comments

--- src/models/Soundscape.ts
+++ src/models/Soundscape.ts
@@ -2,9 +2,14 @@ import { Track } from "./Track";
 import { AudioList } from "../services/audio";
 
 export type Soundscape = {
+  // A unique identifier used in the app to differentiate
+  //  between soundscapes
   index: number,
   name: string,
   tracks: Track[],
+  // A unique identifier used to fetch the details of a
+  //  predefined soundscape. It is `undefined` when a
+  //  soundscape is created from scratch
   sourceId?: string,
   isOpen: boolean,
   audio: AudioList
