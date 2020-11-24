89d9a787df123256ebc0a2d5a671845a2cb39c29
Ben Codrington <benvenda@gmail.com>
2020-11-23 23:17:02 -0500

    Style search result based on object type

--- src/components/SearchDropdown/SearchItem.tsx
+++ src/components/SearchDropdown/SearchItem.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { SearchResult } from "../../models/SearchResult";
 import Tag from "../../widgets/Tag";
+import { OBJECT_TYPES } from "../../models/ObjectTypes";
+
 import "./SearchItem.scss";
 
 type SearchItemProps = {
   data: SearchResult,
   onClick: () => void
+};
+
+const CLASS_NAMES = {
+  [OBJECT_TYPES.LOOP]: 'loop',
+  [OBJECT_TYPES.ONESHOT]: 'one-shot',
+  [OBJECT_TYPES.SOUNDSCAPE]: ''
 };
 
 export default function SearchItem({ data, onClick }: SearchItemProps) {
@@ -14,7 +22,7 @@ export default function SearchItem({ data, onClick }: SearchItemProps) {
       className="search-item-container"
       onClick={onClick}
     >
-      <h4>{data.name}</h4>
+      <h4 className={CLASS_NAMES[data.type] ?? ''}>{data.name}</h4>
       {data.tags.map((tag, index) =>
         <Tag key={index} text={tag} />
       )}
