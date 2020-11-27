252a6a2b8571c5ab847bb9efab3a2ce8ea7d9da8
Ben Codrington <benvenda@gmail.com>
2020-11-26 23:04:39 -0500

    Comment out code to remove

--- src/services/audio.ts
+++ src/services/audio.ts
@@ -15,28 +15,28 @@ function newAudio() {
 // Diffs the passed in `tracks` state with the actual set of howl
 //  objects and corrects differences
 function reconcile(tracks: Track[], audio: AudioList) {
-  const untouchedIndices = Object.keys(audio);
-  tracks.forEach(track => {
-    const { index, trackMetadata } = track;
-    const { name, source } = trackMetadata;
-    const howl = audio[index];
-    if (howl === undefined) {
-      // This track was just added, so create a howl for it
-      console.log(`${name}, ${source.url} was just added, create a howl for it`);
-      // TODO:
-    } else {
-      // Remove this index from the array of indices that haven't
-      //  been inspected during this diff() call
-      untouchedIndices.filter(untouchedIndex => untouchedIndex !== index.toString());
-      // TODO: diff the state of the track with the state of the howl
-    }
-  });
-  // Each index in `untouched` no longer exists in the tracks list,
-  //  and should be destroyed
-  untouchedIndices.forEach(untouchedIndex => {
-    console.log(`index ${untouchedIndex} no longer exists in the tracks list, destroy it.`);
-    // TODO:
-  });
+  // const untouchedIndices = Object.keys(audio);
+  // tracks.forEach(track => {
+  //   const { index, trackMetadata } = track;
+  //   const { name, source } = trackMetadata;
+  //   const howl = audio[index];
+  //   if (howl === undefined) {
+  //     // This track was just added, so create a howl for it
+  //     console.log(`${name}, ${source.url} was just added, create a howl for it`);
+  //     // TODO:
+  //   } else {
+  //     // Remove this index from the array of indices that haven't
+  //     //  been inspected during this diff() call
+  //     untouchedIndices.filter(untouchedIndex => untouchedIndex !== index.toString());
+  //     // TODO: diff the state of the track with the state of the howl
+  //   }
+  // });
+  // // Each index in `untouched` no longer exists in the tracks list,
+  // //  and should be destroyed
+  // untouchedIndices.forEach(untouchedIndex => {
+  //   console.log(`index ${untouchedIndex} no longer exists in the tracks list, destroy it.`);
+  //   // TODO:
+  // });
 };
 
 export default {
