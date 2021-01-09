e094c6f0986cb3d639010f08747883d620220102
Ben Codrington <benvenda@gmail.com>
2021-01-09 12:29:41 -0500

    Fix cors errors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 
 import * as admin from 'firebase-admin';
+const cors = require('cors');
 
 admin.initializeApp();
 const db = admin.firestore();
@@ -9,27 +10,32 @@ const soundscapesRef = db.collection('soundscapes');
 const MAX_RESULTS = 10;
 
 export const searchSoundscapes = functions.https.onRequest(async (request, response) => {
-  const { query } = request;
-  const searchText = query.searchText;
-  functions.logger.info('searchSoundscapes');
-  if (!searchText || searchText.length === 0) {
-    functions.logger.info('Empty searchText, returning.');
-    response.send([]);
-  }
-  functions.logger.info(searchText, { structuredData: true });
-  const results = await soundscapesRef.where('tags', 'array-contains', searchText).limit(MAX_RESULTS).get();
-  if (results.empty) {
-    functions.logger.info('No soundscapes found for query "' + searchText + '".');
-    response.send([]);
-  }
-  functions.logger.info(`Query "${searchText}" returned ${results.docs.length} results.`);
-  const soundscapes = results.docs.map(doc => {
-    const { name, tags } = doc.data();
-    return {
-      id: doc.id,
-      name,
-      tags
-    };
+  return cors()(request, response, async () => {
+
+    const { query } = request;
+    const searchText = query.searchText;
+    functions.logger.info('searchSoundscapes');
+    if (!searchText || searchText.length === 0) {
+      functions.logger.info('Empty searchText, returning.');
+      response.send([]);
+      return;
+    }
+    functions.logger.info(searchText, { structuredData: true });
+    const results = await soundscapesRef.where('tags', 'array-contains', searchText).limit(MAX_RESULTS).get();
+    if (results.empty) {
+      functions.logger.info('No soundscapes found for query "' + searchText + '".');
+      response.send([]);
+      return;
+    }
+    functions.logger.info(`Query "${searchText}" returned ${results.docs.length} results.`);
+    const soundscapes = results.docs.map(doc => {
+      const { name, tags } = doc.data();
+      return {
+        id: doc.id,
+        name,
+        tags
+      };
+    });
+    response.send(soundscapes);
   });
-  response.send(soundscapes);
 });
