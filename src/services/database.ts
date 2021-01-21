4f27e7acb437347b7f1d3d34c90cb9c6758605a3
Ben Codrington <benvenda@gmail.com>
2021-01-20 21:41:39 -0500

    Remove dummy data from database.ts

--- src/services/database.ts
+++ src/services/database.ts
@@ -11,69 +11,6 @@ export type SoundscapeChild = {
   }
 }
 
-// const DUMMY_SOUNDSCAPE_RESULT_DATA: SearchResult[] = [
-//   {
-//     id: 'XXXXX',
-//     name: 'Graveyard',
-//     tags: ['spooky', 'scary', 'eerie', 'haunted', 'ghosts', 'spirits', 'cemetery', 'crypt'],
-//     type: ObjectType.SOUNDSCAPE
-//   },
-//   {
-//     id: 'YYYYY',
-//     name: 'Crowded Tavern',
-//     tags: ['lively', 'ale', 'beer', 'inn', 'warm', 'cozy', 'busy', 'happy', 'drinks', 'merry'],
-//     type: ObjectType.SOUNDSCAPE
-//   }
-// ];
-
-// const DUMMY_TRACK_RESULT_DATA: SearchResult[] = [
-//   {
-//     id: "111111",
-//     name: 'Ominous Ambience',
-//     tags: ['loop', 'spooky', 'graveyard', 'horror', 'ghosts', 'spirits', 'crypt'],
-//     type: ObjectType.LOOP
-//   },
-//   {
-//     id: "222222",
-//     name: 'Carefree Whistling',
-//     tags: ['loop', 'music', 'tinkerer', 'happy', 'pleasant', 'cottage', 'cooking', 'guard'],
-//     type: ObjectType.LOOP
-//   },
-//   {
-//     id: "333333",
-//     name: 'The Jig of Slurs',
-//     tags: ['loop', 'music', 'tavern', 'upbeat', 'jovial', 'celtic', 'happy', 'pleasant', 'fiddle', 'flute', 'merry', 'halfling', 'village', 'town'],
-//     type: ObjectType.LOOP
-//   }
-// ];
-
-// const DUMMY_TRACK_DATA: (LoopData | OneShotData)[] = [
-//   {
-//     id: '111111',
-//     type: ObjectType.LOOP,
-//     fileName: 'ominous-ambience',
-//     name: 'Ominous Ambience',
-//     source: { author: 'Alice', url: 'alice@alice.com' },
-//     tags: ['loop', 'spooky', 'graveyard', 'horror', 'ghosts', 'spirits', 'crypt']
-//   },
-//   {
-//     id: '222222',
-//     type: ObjectType.LOOP,
-//     fileName: 'carefree-whistling',
-//     name: 'Carefree Whistling',
-//     source: { author: 'Bob', url: 'bob@bob.com' },
-//     tags: ['loop', 'music', 'tinkerer', 'happy', 'pleasant', 'cottage', 'cooking', 'guard'],
-//   },
-//   {
-//     id: '333333',
-//     type: ObjectType.LOOP,
-//     fileName: 'jig-of-slurs',
-//     name: 'The Jig of Slurs',
-//     source: { author: 'Charlie', url: 'charlie@charlie.com' },
-//     tags: ['loop', 'music', 'tavern', 'upbeat', 'jovial', 'celtic', 'happy', 'pleasant', 'fiddle', 'flute', 'merry', 'halfling', 'village', 'town'],
-//   }
-// ];
-
 type SoundscapeData = { id: string, tracks: SoundscapeChild[] }
 
 const DUMMY_SOUNDSCAPE_DATA: SoundscapeData[] = [
