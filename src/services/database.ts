4e34c5e7890463e79294900a143af1c808a3321f
Ben Codrington <benvenda@gmail.com>
2021-01-23 16:22:48 -0500

    Trim search text

--- src/services/database.ts
+++ src/services/database.ts
@@ -1,9 +1,13 @@
 import { TrackData } from "../models/DatabaseTypes";
 import { SearchResult } from "../models/SearchResult";
 
-export async function fetchSearchResults(searchText: string) {
+export async function fetchSearchResults(searchText: string): Promise<SearchResult[]> {
+  const trimmedSearchText = searchText.trim();
+  if (trimmedSearchText.length === 0) {
+    return [];
+  }
   // const response = await fetch(`http://localhost:5001/turbo-bard/us-central1/search?searchText=${searchText}`);
-  const response = await fetch(`https://us-central1-turbo-bard.cloudfunctions.net/search?searchText=${searchText}`);
+  const response = await fetch(`https://us-central1-turbo-bard.cloudfunctions.net/search?searchText=${trimmedSearchText}`);
   const results = await response.json();
   return results as SearchResult[];
 }
