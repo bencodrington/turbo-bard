634a1a58a35e46cd51327eb86df3760a543b19ab
Ben Codrington <benvenda@gmail.com>
2021-01-24 17:20:24 -0500

    Display pack size in search results

--- src/components/SearchDropdown/SearchItem.tsx
+++ src/components/SearchDropdown/SearchItem.tsx
@@ -10,13 +10,24 @@ type SearchItemProps = {
 };
 
 export default function SearchItem({ data, onClick }: SearchItemProps) {
+  let packCountElement = null;
+  if (data.tracks !== undefined) {
+    packCountElement = (
+      <span className="pack-count">
+        <span className="pack-count__number">{data.tracks.length}</span>
+      pack
+      </span>
+    );
+  }
+  const filteredTags = data.tags.filter(tag => tag !== 'pack');
   return (
     <li
       className="search-item-container"
       onClick={onClick}
     >
       <h4>{data.name}</h4>
-      {data.tags.map((tag, index) =>
+      {packCountElement}
+      {filteredTags.map((tag, index) =>
         <Tag key={index} text={tag} />
       )}
     </li>
