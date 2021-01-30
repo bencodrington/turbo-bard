2c90ac28db8971bfa72bed1be4aeb0710e8ff53a
Ben Codrington <benvenda@gmail.com>
2021-01-30 13:22:43 -0500

    Add support for enter/space when search item focused

--- src/components/SearchDropdown/SearchItem.tsx
+++ src/components/SearchDropdown/SearchItem.tsx
@@ -20,11 +20,23 @@ export default function SearchItem({ data, onClick }: SearchItemProps) {
     );
   }
   const filteredTags = data.tags.filter(tag => tag !== 'pack');
+
+  function onKeyDown(event: React.KeyboardEvent<HTMLLIElement>) {
+    switch (event.key) {
+      case 'Enter':
+        onClick();
+        break;
+      case ' ':
+        onClick();
+        break;
+    }
+  }
   return (
     <li
       className="search-item-container"
       onClick={onClick}
       tabIndex={0}
+      onKeyDown={onKeyDown}
     >
       <h4>{data.name}</h4>
       {packCountElement}
