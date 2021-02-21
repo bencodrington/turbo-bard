c2b9d8f5b99064a9d883e10a672d5f57105eaacb
Ben Codrington <benvenda@gmail.com>
2021-02-21 14:44:23 -0500

    Label source

--- src/components/TrackList/TrackSource.tsx
+++ src/components/TrackList/TrackSource.tsx
@@ -13,7 +13,7 @@ type TrackSourceProps = {
 export default function TrackSource({ source }: TrackSourceProps) {
   return (
     <div className="track-source-container">
-      <p>{source
+      <p><label>SOURCE</label>{source
         ? source.author ?? ''
         : '...'
       }</p>
