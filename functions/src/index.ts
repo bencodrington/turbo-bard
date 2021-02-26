7ab6ab0f5c8082898a1d381cde84ab7c571203ec
Ben Codrington <benvenda@gmail.com>
2021-02-25 22:41:11 -0500

    Lazy-require Fuse

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,5 @@
-import * as functions from 'firebase-functions';
-
-import * as admin from 'firebase-admin';
+import { https, logger } from 'firebase-functions';
+import { initializeApp, firestore } from 'firebase-admin';
 import Fuse from 'fuse.js';
 
 import { isOneShotData, TrackData } from '../../src/models/DatabaseTypes';
@@ -9,15 +8,15 @@ import { SearchResult } from '../../src/models/SearchResult';
 
 const cors = require('cors');
 
-admin.initializeApp();
-const db = admin.firestore();
+initializeApp();
+const db = firestore();
 const packsRef = db.collection('packs');
 const tracksRef = db.collection('tracks');
 const indexRef = db.collection('index');
 
 const MAX_RESULTS = 25;
 
-const fuseOptions: Fuse.IFuseOptions<SearchResult> = {
+const FUSE_OPTIONS: Fuse.IFuseOptions<SearchResult> = {
   minMatchCharLength: 2,
   ignoreLocation: true,
   threshold: 0.2,
@@ -33,10 +32,12 @@ const fuseOptions: Fuse.IFuseOptions<SearchResult> = {
   ]
 };
 
+let fuse: Fuse<SearchResult>;
+
 async function _search(searchText: string): Promise<SearchResult[]> {
   const index = await indexRef.doc('index').get();
   const { tracks, packs } = index.data() ?? { tracks: [], packs: [] };
-  const fuse = new Fuse([...tracks as SearchResult[], ...packs as SearchResult[]], fuseOptions);
+  fuse = fuse || new Fuse([...tracks as SearchResult[], ...packs as SearchResult[]], FUSE_OPTIONS);
   const words = searchText.split(' ');
   const resultsMap: {
     [id: string]: {
@@ -65,52 +66,50 @@ async function _search(searchText: string): Promise<SearchResult[]> {
     .splice(0, MAX_RESULTS);
 }
 
-export const search = functions.https.onRequest(async (request, response) => {
+export const search = https.onRequest(async (request, response) => {
   return cors()(request, response, async () => {
     const { query } = request;
     const searchText = query.searchText;
-    functions.logger.info('search');
     if (!searchText || searchText.length === 0) {
-      functions.logger.info('Empty searchText, returning.');
+      logger.info('Empty searchText, returning.');
       response.send([]);
       return;
     }
     if (typeof searchText !== 'string') {
-      functions.logger.info('Unsupported searchtext type "', typeof searchText, '", returning.');
+      logger.info('Unsupported searchtext type "', typeof searchText, '", returning.');
       response.send([]);
       return;
     }
-    functions.logger.info(searchText, { structuredData: true });
+    logger.info(searchText, { structuredData: true });
     const results = await _search(searchText);
-    functions.logger.info(`${results.length} results found.`);
+    logger.info(`${results.length} results found.`);
     response.send(results);
   });
 });
 
-export const fetchTrackDataById = functions.https.onRequest(async (request, response) => {
+export const fetchTrackDataById = https.onRequest(async (request, response) => {
   return cors()(request, response, async () => {
 
     const { query } = request;
     const trackId = query.trackId;
-    functions.logger.info('fetchTrackDataById');
     if (!trackId || trackId.length === 0) {
-      functions.logger.info('Empty trackId, returning.');
+      logger.info('Empty trackId, returning.');
       response.send({});
       return;
     }
     if (typeof trackId !== 'string') {
-      functions.logger.info('Invalid trackId, returning.', trackId);
+      logger.info('Invalid trackId, returning.', trackId);
       response.send({});
       return;
     }
-    functions.logger.info(trackId, { structuredData: true });
+    logger.info(trackId, { structuredData: true });
     const result = await tracksRef.doc(trackId).get();
     if (!result.exists) {
-      functions.logger.info('No tracks found for trackId "' + trackId + '".');
+      logger.info('No tracks found for trackId "' + trackId + '".');
       response.send({});
       return;
     }
-    functions.logger.info(`Track with id "${trackId}" found.`);
+    logger.info(`Track with id "${trackId}" found.`);
     const trackData = result.data() as TrackData;
     const { name, source, tags } = trackData;
     if (isOneShotData(trackData)) {
@@ -140,16 +139,16 @@ export const fetchTrackDataById = functions.https.onRequest(async (request, resp
 });
 
 // TODO: call this whenever the database is updated
-export const createSearchIndex = functions.https.onRequest(async (request, response) => {
+export const createSearchIndex = https.onRequest(async (request, response) => {
   return cors()(request, response, async () => {
 
-    functions.logger.info('index packs');
+    logger.info('index packs');
     const packResults = await packsRef.get();
     let packs: SearchResult[] = [];
     if (packResults.empty) {
-      functions.logger.info('No packs found in the packs index.');
+      logger.info('No packs found in the packs index.');
     } else {
-      functions.logger.info(`${packResults.size} packs found.`);
+      logger.info(`${packResults.size} packs found.`);
       packs = packResults.docs.map(doc => {
         const { name, tags, tracks } = doc.data();
         return {
@@ -162,13 +161,13 @@ export const createSearchIndex = functions.https.onRequest(async (request, respo
       });
     }
 
-    functions.logger.info('index tracks');
+    logger.info('index tracks');
     let tracks: SearchResult[] = [];
     const trackResults = await tracksRef.get();
     if (trackResults.empty) {
-      functions.logger.info('No tracks found in the tracks index.');
+      logger.info('No tracks found in the tracks index.');
     } else {
-      functions.logger.info(`${trackResults.size} tracks found.`);
+      logger.info(`${trackResults.size} tracks found.`);
       tracks = trackResults.docs.map(doc => {
         const { name, tags, type } = doc.data();
         return {
