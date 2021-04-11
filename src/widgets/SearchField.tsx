65ffa231f4ae5c468d7f4f56d26fb901300a24ef
Ben Codrington <benvenda@gmail.com>
2021-04-11 11:54:08 -0400

    Focus and highlight searchfield on render

--- src/widgets/SearchField.tsx
+++ src/widgets/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useEffect, useRef } from "react";
 import searchIcon from "../assets/icon-search.svg";
 
 import "./SearchField.scss";
@@ -10,6 +10,16 @@ type SearchFieldProps = {
 };
 
 export default function SearchField(props: SearchFieldProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    // Highlight text the search field's text each time it becomes visible.
+    //  This also applies when switching between global and per-group
+    //  search fields.
+    if (inputRef.current === null) return;
+    inputRef.current.focus();
+    inputRef.current.select();
+  }, []);
 
   function changeHandler(e: ChangeEvent<HTMLInputElement>) {
     props.onChange(e.target.value);
@@ -27,6 +37,7 @@ export default function SearchField(props: SearchFieldProps) {
         placeholder={props.placeholderText}
         value={props.value}
         onChange={changeHandler}
+        ref={inputRef}
         role="searchbox"
         autoFocus
       />
