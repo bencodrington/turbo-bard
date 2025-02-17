95bbea9070689003e2c88fb7649bd1a4e51def77
Ben Codrington <benvenda@gmail.com>
2025-02-17 18:38:43 -0500

    fix: use group's icon and icon colour in now playing banner

--- src/components/NowPlayingBanner.tsx
+++ src/components/NowPlayingBanner.tsx
@@ -9,41 +9,65 @@ import Toggle from "../widgets/Toggle";
 import useBoolean from "../hooks/useBoolean";
 import { useDispatch } from "react-redux";
 import { playGroupSolo } from "../slices/groups";
+import { getIcon, getIconColour } from "../utils/iconUtil";
 
 export default function NowPlayingBanner() {
   const dispatch = useDispatch();
   const groups = useGroups();
-  const [mostRecentlyPlayedGroupIndex, setMostRecentlyPlayedGroupIndex] = useState<number | null>(null);
+  const [mostRecentlyPlayedGroupIndex, setMostRecentlyPlayedGroupIndex] =
+    useState<number | null>(null);
   const playingGroup = getPlayingGroup(groups);
-  const mostRecentlyPlayedGroup = mostRecentlyPlayedGroupIndex === null ? null : (getGroupByIndex(mostRecentlyPlayedGroupIndex, groups) ?? null);
-  const [isCombatToggleChecked, setIsCombatToggleChecked, toggleIsCombatToggleChecked] = useBoolean(false);
+  const mostRecentlyPlayedGroup =
+    mostRecentlyPlayedGroupIndex === null
+      ? null
+      : getGroupByIndex(mostRecentlyPlayedGroupIndex, groups) ?? null;
+  const [
+    isCombatToggleChecked,
+    setIsCombatToggleChecked,
+    toggleIsCombatToggleChecked,
+  ] = useBoolean(false);
   const toggleIsCombatModeActive = () => {
     if (playingGroup === undefined) return;
     const isStartingCombat = isCombatToggleChecked === false;
     toggleIsCombatToggleChecked();
-    dispatch(playGroupSolo({ groupIndex: playingGroup.index, isCombatModeActive: isStartingCombat }));
-  }
+    dispatch(
+      playGroupSolo({
+        groupIndex: playingGroup.index,
+        isCombatModeActive: isStartingCombat,
+      })
+    );
+  };
 
   // When a group starts playing, record its index to populate the name, icon,
   //  etc. and update the combat toggle to be in sync with the store
   useEffect(() => {
     if (playingGroup !== undefined) {
       setMostRecentlyPlayedGroupIndex(playingGroup.index);
-      setIsCombatToggleChecked(playingGroup.combatTracks.some(track => track.isPlaying));
+      setIsCombatToggleChecked(
+        playingGroup.combatTracks.some((track) => track.isPlaying)
+      );
     }
-  }, [playingGroup, setMostRecentlyPlayedGroupIndex, setIsCombatToggleChecked])
+  }, [playingGroup, setMostRecentlyPlayedGroupIndex, setIsCombatToggleChecked]);
 
   const onPlay = () => {
     if (mostRecentlyPlayedGroup === null) return;
-    dispatch(playGroupSolo({ groupIndex: mostRecentlyPlayedGroup.index, isCombatModeActive: isCombatToggleChecked }))
-  }
+    dispatch(
+      playGroupSolo({
+        groupIndex: mostRecentlyPlayedGroup.index,
+        isCombatModeActive: isCombatToggleChecked,
+      })
+    );
+  };
 
   if (mostRecentlyPlayedGroup === null) {
     return null;
   }
   return (
     <div className="now-playing-banner-container">
-      <i className="fa-solid fa-mountain-sun group-list-item-icon" />
+      <i
+        className={`${getIcon(mostRecentlyPlayedGroup)} group-list-item-icon`}
+        style={{ color: getIconColour(mostRecentlyPlayedGroup) }}
+      />
       <span className="environment-name">{mostRecentlyPlayedGroup?.name}</span>
       <Toggle
         id="now-playing-banner-combat-toggle"
@@ -53,7 +77,12 @@ export default function NowPlayingBanner() {
         icon="hand-fist"
         isLabelTextColorSubdued={false}
       />
-      {mostRecentlyPlayedGroup !== null && <PlayGroupButton group={mostRecentlyPlayedGroup} overriddenPlayFunction={onPlay} />}
+      {mostRecentlyPlayedGroup !== null && (
+        <PlayGroupButton
+          group={mostRecentlyPlayedGroup}
+          overriddenPlayFunction={onPlay}
+        />
+      )}
     </div>
-  )
+  );
 }
