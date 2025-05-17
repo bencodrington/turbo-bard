20a788a253dd541eddbc4799921dc9c175144ec1
Ben Codrington <benvenda@gmail.com>
2025-05-17 17:35:06 -0400

    feat: show checkmark in pack search results after adding them

--- src/components/EditableGroup/SearchResults.tsx
+++ src/components/EditableGroup/SearchResults.tsx
@@ -9,38 +9,42 @@ import { SearchResultType } from "../../models/SearchResultType";
 import useBoolean from "../../hooks/useBoolean";
 import Toggle from "../../widgets/Toggle";
 import SearchBar from "../SearchBar";
+import { doesGroupContainSearchResult } from "../../utils/groupUtil";
 
 interface SearchResultsProps {
-  onAddSearchResult: (result: SearchResult, shouldAddToCombatSection: boolean) => void,
-  onCloseSearch: () => void,
-  targetGroupName: string,
-  searchText: string,
-  setSearchText: (newValue: string) => void,
-  searchResultType: SearchResultType,
-  setSearchResultType: (newValue: SearchResultType) => void,
-  isFetchingResults: boolean,
-  results: SearchResult[],
-  targetGroupId: number,
-  soundsInGroup: string[],
+  onAddSearchResult: (
+    result: SearchResult,
+    shouldAddToCombatSection: boolean
+  ) => void;
+  onCloseSearch: () => void;
+  targetGroupName: string;
+  searchText: string;
+  setSearchText: (newValue: string) => void;
+  searchResultType: SearchResultType;
+  setSearchResultType: (newValue: SearchResultType) => void;
+  isFetchingResults: boolean;
+  results: SearchResult[];
+  targetGroupId: number;
+  soundsInGroup: string[];
 }
 
 const TABS: Tab[] = [
   {
     id: SearchResultType.Everything,
-    displayName: 'Everything',
-    icon: 'magnifying-glass'
+    displayName: "Everything",
+    icon: "magnifying-glass",
   },
   {
     id: SearchResultType.Music,
-    displayName: 'Music',
-    icon: 'music'
+    displayName: "Music",
+    icon: "music",
   },
   {
     id: SearchResultType.Ambiance,
-    displayName: 'Ambiance',
-    icon: 'cloud-sun-rain'
+    displayName: "Ambiance",
+    icon: "cloud-sun-rain",
   },
-]
+];
 
 export default function SearchResults({
   onAddSearchResult,
@@ -53,53 +57,61 @@ export default function SearchResults({
   isFetchingResults,
   results,
   targetGroupId,
-  soundsInGroup
+  soundsInGroup,
 }: SearchResultsProps) {
-  const [isAddingToCombatSection, , toggleIsAddingToCombatSection] = useBoolean(false);
+  const [isAddingToCombatSection, , toggleIsAddingToCombatSection] =
+    useBoolean(false);
 
-  const resultElements = results.map(result => (
+  const resultElements = results.map((result) => (
     <SearchItem
       key={result.id}
       data={result}
       onClick={() => onAddSearchResult(result, isAddingToCombatSection)}
-      isAlreadyAdded={soundsInGroup.includes(result.id)}
+      isAlreadyAdded={doesGroupContainSearchResult(soundsInGroup, result)}
     />
   ));
 
-
-  const mainContent = isFetchingResults
-    ? <div className="spinner">
+  const mainContent = isFetchingResults ? (
+    <div className="spinner">
       <i className="fa-solid fa-circle-notch fa-spin" />
     </div>
-    : resultElements.length > 0
-      ? <ul className="results">{resultElements}</ul>
-      : searchText.length === 0
-        ? null
-        : <p className="message">Couldn't find those sounds.</p>;
+  ) : resultElements.length > 0 ? (
+    <ul className="results">{resultElements}</ul>
+  ) : searchText.length === 0 ? null : (
+    <p className="message">Couldn't find those sounds.</p>
+  );
 
   return (
     <div className="search-results-container">
       <header>
         <div className="title-row">
-          <Button
-            icon="arrow-left"
-            onClick={onCloseSearch}
-          />
-          <span>Adding to <span className="target-group-name">{targetGroupName}</span>
+          <Button icon="arrow-left" onClick={onCloseSearch} />
+          <span>
+            Adding to{" "}
+            <span className="target-group-name">{targetGroupName}</span>
           </span>
-          <Toggle id="combat-toggle" label="Add to Combat" isChecked={isAddingToCombatSection} onToggle={toggleIsAddingToCombatSection} icon="hand-fist" />
+          <Toggle
+            id="combat-toggle"
+            label="Add to Combat"
+            isChecked={isAddingToCombatSection}
+            onToggle={toggleIsAddingToCombatSection}
+            icon="hand-fist"
+          />
         </div>
-        <SearchBar searchText={searchText} setSearchText={setSearchText} placeholder="Search all sounds" />
+        <SearchBar
+          searchText={searchText}
+          setSearchText={setSearchText}
+          placeholder="Search all sounds"
+        />
         <TabSwitcher
           selectedTabId={searchResultType}
           tabs={TABS}
-          onTabClick={newTabId => setSearchResultType(newTabId as SearchResultType)}
+          onTabClick={(newTabId) =>
+            setSearchResultType(newTabId as SearchResultType)
+          }
         />
       </header>
-      {
-        mainContent
-      }
-
+      {mainContent}
     </div>
-  )
+  );
 }
--- src/utils/groupUtil.ts
+++ src/utils/groupUtil.ts
@@ -1,4 +1,6 @@
 import { Group } from "../models/Group";
+import { ObjectType } from "../models/ObjectTypes";
+import { SearchResult } from "../models/SearchResult";
 
 export function findTrackInGroup(trackIndex: number, group: Group) {
   return [...group.tracks, ...group.combatTracks].find(
@@ -8,4 +10,17 @@ export function findTrackInGroup(trackIndex: number, group: Group) {
 
 export function isGroupPlayable(group: Group) {
   return group.tracks.length > 0 || group.combatTracks.length > 0;
+}
+
+export function doesGroupContainSearchResult(
+  trackIdsInGroup: string[],
+  result: SearchResult
+) {
+  if (result.type === ObjectType.PACK) {
+    // Return true if the group already contains every track in the pack
+    return (result.tracks ?? []).every(({ id }) =>
+      trackIdsInGroup.includes(id)
+    );
+  }
+  return trackIdsInGroup.includes(result.id);
 }
